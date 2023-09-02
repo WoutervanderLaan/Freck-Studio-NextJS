b059452ce8331b961e14bd0af655dab91482f1c1
Wouter van der Laan <woutervanderlaan@MacBook-Pro.local>
2023-09-02 15:10:12 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ const HomePage = () => {
     return (
         <div className="w-full h-[200px] bg-blue">
             <h1 className="font-cursive text-2xl font-black">FRECK STUDIO</h1>
+            <h1 className="font-cursive text-2xl font-black">FRECK STUDIO</h1>
+            <h1 className="font-cursive text-2xl font-black">FRECK STUDIO</h1>
         </div>
     )
 }
