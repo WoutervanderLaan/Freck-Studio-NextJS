d9baf71a077355cf205652d0978b0c22d787e13c
Wouter van der Laan <woutervanderlaan@MacBook-Pro.local>
2023-09-13 09:25:55 +0200

    another test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Intro from '@/components/sections/Intro'
 import WhoCard from '@/components/sections/WhoCard'
-// import Card from '@/components/Card'
-import Brands from '@/components/sections/Brands'
+import Card from '@/components/Card'
+// import Brands from '@/components/sections/Brands'
 import Testimonials from '@/components/sections/Testimonials'
 
 const HomePage = () => {
@@ -9,13 +9,13 @@ const HomePage = () => {
         <>
             <Intro />
 
-            <Brands />
+            {/* <Brands /> */}
 
             <section id="about" className="container">
                 <WhoCard />
             </section>
 
-            {/* <section
+            <section
                 id="why"
                 className="container flex flex-col gap-10 lg:flex-row"
             >
@@ -31,7 +31,7 @@ const HomePage = () => {
                 >
                     <div className="bg-white w-[60%] aspect-square rounded-full" />
                 </Card>
-            </section> */}
+            </section>
 
             <Testimonials />
         </>
